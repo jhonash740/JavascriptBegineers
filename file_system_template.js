b4ba9226f9b122e6395ece6d29486c6ebe65c668
Lakshmi54 <91776607+Lakshmi54@users.noreply.github.com>
2021-10-02 13:06:50 +0530

    Added 400 port

--- file_system_template.js
+++ file_system_template.js
@@ -4,6 +4,7 @@ http.createServer(function (req, res) {
   //Open a file on the server and return its content:
   fs.readFile('demofile1.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writ( 400, 'text/html' );
     res.write(data);
     return res.end();
   });
